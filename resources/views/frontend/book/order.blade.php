2c22309b0422da40a0c077cd42a9c37d280830d9
Princebabariya7 <babariyaprince042@gmail.com>
2024-01-30 12:28:11 +0530

    [FD] done changes in slote booking field

--- resources/views/frontend/book/order.blade.php
+++ resources/views/frontend/book/order.blade.php
@@ -95,16 +95,19 @@
                                 <h5 class="modal-title" id="timeSlotModalLabel">Select Time Slot</h5>
                             </div>
                             <div class="modal-body">
-                                @php
-                                    $timeSlots = ['9:00 AM - 10:00 AM','10:00 AM - 11:00 AM','11:00 AM - 12:00 PM','12:00 PM - 1:00 PM','1:00 PM - 2:00 PM','2:00 PM - 3:00 PM','3:00 PM - 4:00 PM','4:00 PM - 5:00 PM','5:00 PM - 6:00 PM','6:00 PM - 7:00 PM','7:00 PM - 8:00 PM','8:00 PM - 9:00 PM', ];
-                                @endphp
-                                <ul class="list-group">
-                                    @foreach($timeSlots as $timeSlot)
-                                        <li class="list-group-item" onclick="selectTimeSlot('{{ $timeSlot }}')">
-                                            {{ $timeSlot }}
-                                        </li>
-                                    @endforeach
-                                </ul>
+                                <form id="timeSlotForm">
+                                    <ul class="list-group">
+                                        @foreach($timeSlots as $key => $timeSlot)
+                                            <li class="list-group-item">
+                                                <label>
+                                                    <input type="radio" name="time-slot" value="{{ $key }}"
+                                                           onclick="selectTimeSlot('{{ $timeSlot }}')">
+                                                    {{ $timeSlot }}
+                                                </label>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -140,7 +143,6 @@
             $('#reservationdate').datetimepicker({
                 format: 'L',
                 minDate: moment(),
-
             });
         })
 
@@ -168,7 +170,6 @@
                                 .attr("value", key)
                                 .addClass('options-' + key)
                                 .text(value));
-
                             @if($editMode)
                             if (key == {{$service_id}})
                             {
