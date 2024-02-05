4f93393a12a125a1b9d18ecfdc10b242e4e95b2b
Yash <yash@shreets.com>
2024-02-05 17:39:21 +0530

    [BK] Changes Payment Ui

--- resources/views/frontend/payment/index.blade.php
+++ resources/views/frontend/payment/index.blade.php
@@ -10,135 +10,116 @@
             </div>
         </div>
     </div>
-    <div class="container appointment">
+    <div class="container appointment  mt-3">
         <div class="card login_content payment_formpage">
-            <div class="card-body register-card-body">
-                <div class="card card-secondary">
-                    <div class="card-header">
-                        <h3 class="card-title mb-0">Selected Servies</h3>
-                    </div>
-                    <div class="card-body table-responsive p-0">
-                        <table class="table table-hover text-nowrap">
-                            <thead>
-                            <tr>
-                                <th>service</th>
-                                <th>price</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>John Doe</td>
-                                <td><i class="fa fa-inr" aria-hidden="true"></i>
-                                    100
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Alexander Pierce</td>
-                                <td><i class="fa fa-inr" aria-hidden="true"></i>
-                                    200
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <form action="{{route('payment.info.store')}}" method="post">
-                @csrf
-                <div class="container main_form">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-
+            {!! Form::open(['route' => 'payment.info.store', 'method' => 'post']) !!}
+            @csrf
+            <div class="container main_form pt-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="Billing">Buyer name</label>
+                                            {!! Form::text('buyer_name', null, ['class' => 'form-control', 'id' => 'Billing', 'placeholder' => 'Enter your name']) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="Billing_email">Buyer email</label>
+                                            {!! Form::text('buyer_email', null, ['class' => 'form-control', 'id' => 'Billing_email', 'placeholder' => 'Enter your email']) !!}
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label for="Billing">Buyer name</label>
-                                                <input type="text" class="form-control" id="Billing"
-                                                       placeholder="Enter your name" name="buyer_name">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label for="Billing_email">Buyer email</label>
-                                                <input type="text" class="form-control" id="Billing_email"
-                                                       placeholder="Enter your email" name="buyer_email">
-                                            </div>
+                                    <label for="Buyer_Address">Buyer Address</label>
+                                    {!! Form::textarea('buyer_address', null, ['class' => 'form-control', 'id' => 'Buyer_Address', 'rows' => 2]) !!}
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="cardnumber">Enter card number</label>
+                                            {!! Form::text('cd_number', null, ['class' => 'form-control', 'id' => 'cardnumber']) !!}
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="Buyer_Address">Buyer Address</label>
-                                        <textarea id="Buyer_Address" class="form-control" rows="2"
-                                                  name="buyer_address"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="cardnumber">Enter card number</label>
-                                        <input type="text" id="cardnumber" class="form-control" name="cd_number">
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-8">
-                                            <label>expiry date</label>
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <select class="form-control select2bs4" style="width: 100%;"
-                                                                name="month">
-                                                            <option selected disabled>month</option>
-                                                            <option>1</option>
-                                                            <option>2</option>
-                                                            <option>3</option>
-                                                            <option>4</option>
-                                                            <option>5</option>
-                                                            <option>6</option>
-                                                            <option>7</option>
-                                                            <option>8</option>
-                                                            <option>9</option>
-                                                            <option>10</option>
-                                                            <option>11</option>
-                                                            <option>12</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <select class="form-control select2bs4" style="width: 100%;"
-                                                                name="year">
-                                                            <option selected disabled>year</option>
-                                                            <option>1</option>
-                                                            <option>2</option>
-                                                            <option>3</option>
-                                                            <option>4</option>
-                                                            <option>5</option>
-                                                            <option>6</option>
-                                                            <option>7</option>
-                                                            <option>8</option>
-                                                            <option>9</option>
-                                                            <option>10</option>
-                                                            <option>11</option>
-                                                            <option>12</option>
-                                                        </select>
-                                                    </div>
+                                    <div class="col-md-4">
+                                        <label>Expiry Date</label>
+                                        <div class="form-group">
+                                            <div class="input-group date" id="reservationdate"
+                                                 data-target-input="nearest">
+                                                {!! Form::text('date', null, ['class' => 'form-control appointment-date datetimepicker-input', 'id' => 'date', 'data-target' => '#reservationdate', 'autocomplete' => 'off']) !!}
+                                                <div class="input-group-append" data-target="#reservationdate"
+                                                     data-toggle="datetimepicker">
+                                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label for="cvv">cvv</label>
-                                                <input type="text" id="cvv" class="form-control" name="cvv">
-                                            </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="cvv">CVV</label>
+                                            {!! Form::text('cvv', null, ['class' => 'form-control', 'id' => 'cvv']) !!}
                                         </div>
                                     </div>
-                                    <button type="submit" class="btn btn-primary float-right"
-                                            style="margin-right: 5px;">
-                                        MAKE PAYMENT
-                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </form>
+
+                <!-- Selected Services Section -->
+                <div class="card-body register-card-body">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title mb-0">Selected Services</h3>
+                        </div>
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                <tr>
+                                    <th>Service</th>
+                                    <th>Price</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>John Doe</td>
+                                    <td><i class="fa fa-inr" aria-hidden="true"></i> 100</td>
+                                </tr>
+                                <tr>
+                                    <td>Alexander Pierce</td>
+                                    <td><i class="fa fa-inr" aria-hidden="true"></i> 200</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Payment Methods Section -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row payment_methods">
+                                <!-- Payment methods images -->
+                                <div class="col-md">
+                                    <img src="{{ asset('cd/page_img/credit/visa.png') }}" alt="Visa">
+                                    <img src="{{ asset('cd/page_img/credit/mastercard.png') }}" alt="MasterCard">
+                                    <img src="{{ asset('cd/page_img/credit/american-express.png') }}"
+                                         alt="American Express">
+                                    <img src="{{ asset('cd/page_img/credit/paypal2.png') }}" alt="Paypal">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            {!! Form::submit('MAKE PAYMENT', ['class' => 'btn btn-sm btn-primary float-right', 'style' => 'margin-right: 5px; float: left; margin-bottom: 20px;']) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
@@ -150,10 +131,13 @@
         @endforeach
         @endif
 
-        @if ($errors->any())
-        @foreach ($errors->all() as $error)
-        toastr.error('{{ $error }}');
-        @endforeach
-        @endif
+        $(document).ready(function () {
+            $('#reservationdate').datetimepicker({
+                format: 'MM/YYYY',
+                viewMode: 'months',
+                minViewMode: 'months',
+                minDate: moment(),
+            });
+        });
     </script>
 @endsection
