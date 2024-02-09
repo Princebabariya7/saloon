9f4fd9639b455b302dc0d2754cc74a0a8ce01f6b
Princebabariya7 <babariyaprince042@gmail.com>
2024-02-09 10:09:40 +0530

    [FD] payment related observer and event

--- app/Observers/PaymentObserver.php
+++ app/Observers/PaymentObserver.php
@@ -2,12 +2,7 @@
 
 namespace App\Observers;
 
-use App\Mail\OrderMail;
-use App\Models\Appointment;
-use App\Models\AppointmentSlot;
 use App\Models\Payment;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Mail;
 
 class PaymentObserver
 {
@@ -15,7 +10,7 @@ class PaymentObserver
     {
         try
         {
-       //
+          //
         }
         catch (\Exception $e)
         {
