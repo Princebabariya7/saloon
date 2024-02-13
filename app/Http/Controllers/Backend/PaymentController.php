61782ee6f833c9248e90998a8db0b08828fbcab9
Yash <yash@shreets.com>
2024-02-13 10:39:27 +0530

    [BK] Changes in Payment Controller For Json response

--- app/Http/Controllers/Backend/PaymentController.php
+++ app/Http/Controllers/Backend/PaymentController.php
@@ -36,8 +36,7 @@ class PaymentController extends Controller
 
     public function store(PaymentStoreRequest $request)
     {
-        try
-        {
+        try {
             Stripe::setApiKey(config('services.stripe.secret'));
             $intent = PaymentIntent::create([
                 'amount'               => 821275,
@@ -45,23 +44,27 @@ class PaymentController extends Controller
                 'payment_method_types' => ['card'],
                 'payment_method_data'  => ['type' => 'card', 'card' => ['token' => $request->stripeToken]]
             ]);
+
+            $transactionDetail = json_encode(['status' => true, 'message' => 'Payment Was Successfully']);
+
             Payment::create([
-                'buyer_name'  => $request->buyer_name,
-                'buyer_email' => $request->buyer_email,
+                'buyer_name'         => $request->buyer_name,
+                'buyer_email'        => $request->buyer_email,
                 'transaction_id'     => $request->stripeToken,
-                'transaction_detail' => 'ok',
+                'transaction_detail' => $transactionDetail,
                 'gateway'            => 'Stripe',
                 'appointment_id'     => $request->token,
                 'status'             => 'Pending',
-                'updated_at'  => now(),
-                'created_at'  => now(),
+                'updated_at'         => now(),
+                'created_at'         => now(),
             ]);
+
             session()->put('msg', 'payment accepted');
             return response()->json(['status' => true, 'message' => 'Payment Was Successfully', 'url' => route('admin.appointment.index')], 200);
+        } catch (Exception $e) {
+            // Handle exceptions here
+            return response()->json(['status' => false, 'message' => 'Payment failed', 'error' => $e->getMessage()], 500);
         }
-        catch (\Exception $e)
-        {
-            dd($e->getMessage());
-        }
+
     }
 }
