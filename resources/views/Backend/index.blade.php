a67037da6b5e5bfdfc1cd3f76a904d5bf04bed22
Yash <yash@shreets.com>
2024-02-14 15:45:45 +0530

    [BK] changes in Dashboard blade file

--- resources/views/Backend/index.blade.php
+++ resources/views/Backend/index.blade.php
@@ -34,7 +34,7 @@
                             <div class="icon">
                                 <i class="ion ion-bag"></i>
                             </div>
-                            <a href="{{ route('admin.order.details') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="{{ route('admin.appointment.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-6">
@@ -46,7 +46,7 @@
                             <div class="icon">
                                 <i class="ion ion-stats-bars"></i>
                             </div>
-                            <a href="{{ route('admin.appointment.details') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="{{ route('admin.appointment.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
 
