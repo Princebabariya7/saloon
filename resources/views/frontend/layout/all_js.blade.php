05f75cc06c14f7b95f76d6ef243ad93b5cdd781e
Yash <yash@shreets.com>
2024-02-15 11:54:17 +0530

    [BK] changes in head file for fix collapse open and close problem

--- resources/views/frontend/layout/all_js.blade.php
+++ resources/views/frontend/layout/all_js.blade.php
@@ -1,4 +1,3 @@
-
 {{--<script src="{{asset('cd/lib/easing/jquery-3.4.1.min.js')}}"></script>--}}
 
 {{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>--}}
