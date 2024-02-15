59fd95b3bd7e644b12c85b4261d0654c3e1ae1b0
Yash <yash@shreets.com>
2024-02-15 11:53:34 +0530

    [BK] changes in head file for fix collapse open and close problem

--- resources/views/frontend/layout/all_js.blade.php
+++ resources/views/frontend/layout/all_js.blade.php
@@ -1,8 +1,46 @@
 
-<script src="{{asset('cd/lib/easing/jquery-3.4.1.min.js')}}"></script>
+{{--<script src="{{asset('cd/lib/easing/jquery-3.4.1.min.js')}}"></script>--}}
 
-{{--<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>--}}
+{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>--}}
+{{--<script src="{{asset('cd/lib/easing/easing.min.js')}}"></script>--}}
+{{--<script src="{{asset('cd/lib/owlcarousel/owl.carousel.min.js')}}"></script>--}}
+{{--<script src="{{asset('cd/lib/isotope/isotope.pkgd.min.js')}}"></script>--}}
+{{--<script src="{{asset('cd/lib/lightbox/js/lightbox.min.js')}}"></script>--}}
+
+{{--<script src="{{asset('cd/mail/jqBootstrapValidation.min.js')}}"></script>--}}
+{{--<script src="{{asset('cd/assets/js/adminlte.min.js')}}"></script>--}}
+
+{{--<!-- Template Javascript -->--}}
+{{--<script src="{{asset('cd/js/main.js')}}"></script>--}}
+{{--<script src="{{asset('cd/assets/jquery/jquery.min.js')}}"></script>--}}
+{{--<!-- Bootstrap 4 -->--}}
+{{--<script src="{{asset('cd/assets/bootstrap/js/bootstrap.bundle.min.js')}}"></script>--}}
+{{--<!-- Select2 -->--}}
+{{--<script src="{{asset('cd/assets/select2/js/select2.full.min.js')}}"></script>--}}
+{{--<!-- Bootstrap4 Duallistbox -->--}}
+{{--<script src="{{asset('cd/assets/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js')}}"></script>--}}
+{{--<!-- InputMask -->--}}
+{{--<script src="{{asset('cd/assets/moment/moment.min.js')}}"></script>--}}
+{{--<script src="{{asset('cd/assets/inputmask/jquery.inputmask.min.js')}}"></script>--}}
+{{--<!-- date-range-picker -->--}}
+{{--<script src="{{asset('cd/assets/daterangepicker/daterangepicker.js')}}"></script>--}}
+{{--<script src="{{asset('cd/assets/plugins/toastr/toastr.min.js')}}"></script>--}}
+{{--<!-- Tempusdominus Bootstrap 4 -->--}}
+{{--<script src="{{asset('cd/assets/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>--}}
+{{--<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>--}}
+
+
+{{--<script src="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>--}}
+{{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>--}}
+{{--@yield('custom_js')--}}
+
+<!-- Load jQuery -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+<!-- Bootstrap 4 -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+
+<!-- Other scripts -->
 <script src="{{asset('cd/lib/easing/easing.min.js')}}"></script>
 <script src="{{asset('cd/lib/owlcarousel/owl.carousel.min.js')}}"></script>
 <script src="{{asset('cd/lib/isotope/isotope.pkgd.min.js')}}"></script>
@@ -15,24 +53,33 @@
 
 <!-- Template Javascript -->
 <script src="{{asset('cd/js/main.js')}}"></script>
-<script src="{{asset('cd/assets/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('cd/assets/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
 <!-- Select2 -->
 <script src="{{asset('cd/assets/select2/js/select2.full.min.js')}}"></script>
+
 <!-- Bootstrap4 Duallistbox -->
 <script src="{{asset('cd/assets/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js')}}"></script>
+
 <!-- InputMask -->
 <script src="{{asset('cd/assets/moment/moment.min.js')}}"></script>
 <script src="{{asset('cd/assets/inputmask/jquery.inputmask.min.js')}}"></script>
+
 <!-- date-range-picker -->
 <script src="{{asset('cd/assets/daterangepicker/daterangepicker.js')}}"></script>
+
+<!-- Toastr -->
 <script src="{{asset('cd/assets/plugins/toastr/toastr.min.js')}}"></script>
+
 <!-- Tempusdominus Bootstrap 4 -->
 <script src="{{asset('cd/assets/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+
+<!-- SweetAlert2 -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-
+<!-- Bootstrap Datepicker -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>
+
+<!-- Bootstrap 5 -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
 @yield('custom_js')
