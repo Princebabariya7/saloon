5f06261cf88cba0e9816c36b50aa950a76919fb0
Princebabariya7 <babariyaprince042@gmail.com>
2024-02-16 14:45:41 +0530

    [FD] email fild added in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,19 +8,20 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table)
+        {
             $table->id();
-            $table->string('firstname',50);
-            $table->string('lastname',50);
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('gender',['m','f'])->nullable();
-            $table->string('mobile',10)->unique()->nullable();
-            $table->string('address',100)->nullable();
-            $table->string('city',20)->nullable();
-            $table->string('state',20)->nullable();
+            $table->enum('gender', ['m', 'f'])->nullable();
+            $table->string('mobile', 10)->unique()->nullable();
+            $table->string('address', 100)->nullable();
+            $table->string('city', 20)->nullable();
+            $table->string('state', 20)->nullable();
             $table->integer('zipcode')->nullable();
-            $table->enum('user_status',['Admin','User']);
+            $table->enum('user_status', ['Admin', 'User']);
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
