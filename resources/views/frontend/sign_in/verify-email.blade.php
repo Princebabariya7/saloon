6d3c38538b15df8f60d4bafdf2db3ff58444bc5e
Princebabariya7 <babariyaprince042@gmail.com>
2024-02-20 19:17:21 +0530

    [FD] formate related changes

--- resources/views/frontend/sign_in/verify-email.blade.php
+++ resources/views/frontend/sign_in/verify-email.blade.php
@@ -10,7 +10,7 @@
 <div class="page-header m-0">
     <div class="container">
         <div class="row justify-content-around">
-            <h1 class="login_logo font-weight-normal">Verify Your Email</h1>
+            <h1 class="login_logo font-weight-normal">Please Verify Your Email</h1>
         </div>
     </div>
 </div>
