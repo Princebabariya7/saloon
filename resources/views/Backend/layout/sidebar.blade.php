2179421554e09d43ff1d66169894d0bd2d1cc8a7
Yash <yash@shreets.com>
2024-02-21 12:44:41 +0530

    [BK] Changes In sidebar.blade.php

--- resources/views/Backend/layout/sidebar.blade.php
+++ resources/views/Backend/layout/sidebar.blade.php
@@ -1,12 +1,8 @@
-<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <a href="{{route('dashboard.index')}}" class="brand-link">
         <h2 class="text-center saloon_logo">Hairec Saloon</h2>
     </a>
-    <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="{{ asset('backend/assets/img/testimonial-3.jpg') }}" class="img-circle elevation-2"
@@ -16,7 +12,6 @@
                 <a href="{{ route('user.profile') }}" class="d-block">{{ auth()->user()->firstname }}</a>
             </div>
         </div>
-        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item {{ Route::is('dashboard.index') ? 'menu-open' : '' }}">
@@ -84,7 +79,5 @@
                 </li>
             </ul>
         </nav>
-        <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
 </aside>
