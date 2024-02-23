0cc7fa7ff0a35d4e9e7b6a05c8fed754489a5d78
Princebabariya7 <babariyaprince042@gmail.com>
2024-02-23 10:16:26 +0530

    [FD]  done changes for commnd and schedule for appointment remainder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendEmail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,11 +10,10 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:email')->everyMinute();
+        $schedule->command('send:email')->everyTenMinutes();
     }
 
     protected $commands = [
-        \App\Console\Commands\SendEmail::class,
-
+        SendEmail::class,
     ];
 }
