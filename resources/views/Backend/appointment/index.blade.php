c9fbaa561f46549e285f774e36f557647665d4e5
Princebabariya7 <babariyaprince042@gmail.com>
2024-02-28 19:09:07 +0530

    [FD] pagination related changes

--- resources/views/Backend/appointment/index.blade.php
+++ resources/views/Backend/appointment/index.blade.php
@@ -277,7 +277,7 @@
 
                     <div class="card-footer">
                         <div class="pagination pagination-sm float-right">
-                            {{ $appointments->links() }}
+                            {{ $appointments->appends(request()->except('page'))->render() }}
                         </div>
                         <div class="clear-btn">
                             @if(request('search') != '' || request('status') != '' || request('type') != '' || request('anotherInput') != '')
