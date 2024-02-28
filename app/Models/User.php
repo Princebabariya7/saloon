c4f75e70d3f3d674882f9c26475023f06043dbb7
Yash <yash@shreets.com>
2024-02-28 10:14:57 +0530

    [BK] changes in USer MOdel For Sortable

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Kyslik\ColumnSortable\Sortable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, Sortable;
 
     protected $table = 'users';
 
@@ -39,5 +40,29 @@ class User extends Authenticatable
     public function appointments()
     {
         return $this->hasMany(AppointmentSlot::class, 'user_id', 'id');
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        if ($search)
+        {
+            $query->Where('firstname', 'LIKE', '%' . $search . '%');
+            $query->orwhere('lastname', 'LIKE', '%' . $search . '%');
+            $query->orwhere('email', 'LIKE', '%' . $search . '%');
+            $query->orwhere('mobile', 'LIKE', '%' . $search . '%');
+            $query->orwhere('user_status', 'LIKE', '%' . $search . '%');
+        }
+        return $query;
+    }
+
+    public function scopeStatus($query, $status)
+    {
+
+        if ($status)
+        {
+            $query->where('user_status', $status);
+        }
+
+        return $query;
     }
 }
