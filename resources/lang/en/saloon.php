2fbb3270743b8c26174ce2df8e873e6f3ab32a36
Yash <yash@shreets.com>
2024-03-05 14:45:34 +0530

    [BK] Language Related Changes

---
+++ resources/lang/en/saloon.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'action'       => 'Action',
+    'user_name'    => 'Username',
+    'category'     => 'Category',
+    'type'         => 'Type',
+    'date'         => 'Date',
+    'time'         => 'Time',
+    'app_date'     => 'App. Date',
+    'booking_date' => 'Booking Date',
+    'status'       => 'Status',
+    'success'      => 'Your appointment is successfully added!!',
+];
