96e36b6c88bc758f2175b576a92a2eed7f6099fd
Princebabariya7 <babariyaprince042@gmail.com>
2024-03-07 11:47:10 +0530

    [FD] remove user id

--- resources/views/Frontend/layout/all_js.blade.php
+++ resources/views/Frontend/layout/all_js.blade.php
@@ -45,5 +45,6 @@
 
 <!-- Bootstrap 5 -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 @yield('custom_js')
