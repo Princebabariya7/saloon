8df1b0b72e5b3a3f1b3e957c95381af49b4b6453
Princebabariya7 <babariyaprince042@gmail.com>
2024-03-11 10:55:58 +0530

    [FD] forgot password

--- resources/views/Frontend/sign_in/forgetPassword.blade.php
+++ resources/views/Frontend/sign_in/forgetPassword.blade.php
@@ -17,7 +17,7 @@
 {{--            <div class="form-group">--}}
 {{--                {!! Form::text('email',null, ['class' => 'form-control item','placeholder' => __('saloon.enter_your_email'), 'autocomplete' => 'off']) !!}--}}
 {{--            </div>--}}
-{{--            --}}{{--<div class="form-group">--}}
+{{--            <div class="form-group">--}}
 {{--                {!! Form::password('password',['class' => 'form-control item','placeholder' =>  __('saloon.enter_password')]) !!}--}}
 {{--            </div>--}}
 {{--            <div class="form-group">--}}
@@ -61,41 +61,42 @@
             </div>
         </div>
     </div>
-<main class="login-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Reset Password</div>
-                    <div class="card-body">
-                        @if (Session::has('message'))
-                            <div class="alert alert-success" role="alert">
-                                {{ Session::get('message') }}
-                            </div>
-                        @endif
-
-                        <form action="{{ route('forget.password.post') }}" method="POST">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
+    <main class="login-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card m-0">
+                        <div class="card-header">Reset Password</div>
+                        <div class="card-body">
+                            @if (Session::has('message'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ Session::get('message') }}
                                 </div>
-                            </div>
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </form>
-
+                            @endif
+                            <form action="{{ route('forget.password.post') }}" method="POST">
+                                @csrf
+                                <div class="form-group row">
+                                    <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail
+                                        Address</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="email_address" class="form-control" name="email" required
+                                               autofocus autocomplete="off">
+                                        @if ($errors->has('email'))
+                                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Send Password Reset Link
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
+
 @endsection
