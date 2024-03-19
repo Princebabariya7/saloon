c2c5a30f36bc65f8d46a7ab9888db0ddef07bd10
Princebabariya7 <137056069+Princebabariya7@users.noreply.github.com>
2024-03-19 10:06:33 +0530

    Update index.blade.php by prince

--- resources/views/Frontend/payment/index.blade.php
+++ resources/views/Frontend/payment/index.blade.php
@@ -78,7 +78,7 @@
             });
         });
         $(document).ready(function () {
-            var stripe = Stripe('pk_test_51Oh3DySJVbxBeWNZmETZLr6xYA9iBDWcRZtwVNuuER07GavyTFefKgr1UzZiQ66cxfafkN62lfooMCp3TgaarT9o00VpYkC88e');
+            var stripe = Stripe('please enter your sripe key');
             var elements = stripe.elements();
             var paymentElement = elements.create('card');
             paymentElement.mount('#payment-element');
